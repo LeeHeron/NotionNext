00a4f31a92079517f2924a4a285096f6906cfe34
LeeHeron <foinyong@gmail.com>
2025-04-25 21:01:12 +0800

    Add files via upload

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
     PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
-    PROXIO_WELCOME_TEXT: '成长有迹可循，爱意无限延伸', 
+    PROXIO_WELCOME_TEXT: '成长有迹可循\n爱意无限延伸', 
 
     // 英雄区块导航
     PROXIO_HERO_ENABLE: true, // 开启英雄区
@@ -12,7 +12,7 @@ const CONFIG = {
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
     PROXIO_HERO_BUTTON_1_TEXT: '进入私密专区', // 英雄区按钮
     PROXIO_HERO_BUTTON_1_URL:
-        'https://mvwebfs.tx.kugou.com/202504231219/ce55c363a9493022621de81f642fb488/v2/b0e151c356174bdd7c21155b4769bc18/G099/M08/1C/19/A4cBAFjnOQyAf1KOBZRArKAG1sQ795.mp4', // 英雄区按钮
+        'https://mvwebfs.kugou.com/202504242228/cfc225fc895427b11ef85c42d42cf7c0/v2/b0e151c356174bdd7c21155b4769bc18/G099/M08/1C/19/A4cBAFjnOQyAf1KOBZRArKAG1sQ795.mp4', // rick
     PROXIO_HERO_BUTTON_2_TEXT: '与诣宝一起玩游戏', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_URL: 'https://ys-api.mihoyo.com/event/download_porter/link/ys_cn/official/pc_default', // 英雄区按钮
     PROXIO_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
@@ -61,7 +61,7 @@ const CONFIG = {
     PROXIO_FEATURE_3_TEXT_1: '表情包轰炸+深夜小作文，群聊冷场终结者！',
 
     PROXIO_FEATURE_BUTTON_TEXT: '了解更多', // 按钮文字
-    PROXIO_FEATURE_BUTTON_URL: 'https://mvwebfs.kugou.com/202504221935/b8afcf6d37a1cf18e802cbed6b7a4c21/KGTX/CLTX002/5a6e9d1b6c4851280043274d4e4270b7.mp4', // 按钮跳转
+    PROXIO_FEATURE_BUTTON_URL: 'https://mvwebfs.tx.kugou.com/202504242230/658142cc8a796adae05aacb1ce392229/v2/f00858ff8061e523f883f4b71732f95d/G131/M01/1F/0F/ww0DAFretiSAKZ2jB_fgWyX-aNU408.mp4', // 阳光彩虹小白马
 
     // 首页生涯区块
     PROXIO_CAREER_ENABLE: true, // 区块开关
@@ -71,9 +71,9 @@ const CONFIG = {
 
     // 生涯内容卡牌 ，title是标题 ，bio是备注，text是详情
     PROXIO_CAREERS: [
-        { title: 'Freelance Architect', bio: '2016-2020', text: 'As a freelance architect, I worked on a range of residential and commercial projects, balancing form and function. I collaborated with clients and contractors to transform concepts into reality, ensuring each design was both aesthetically pleasing and structurally sound.' },
-        { title: 'Product Designer at Spotify', bio: '2020-2022', text: 'At Spotify, I helped shape innovative features for millions of users globally. My focus was on creating seamless music discovery experiences, enhancing user interfaces, and optimizing cross-platform navigation, which led to an improved product flow and increased user satisfaction.' },
-        { title: 'Freelance Product Designer', bio: '2022-Now', text: 'Now I design user-centric products that align with client visions. As a freelance product designer, I collaborate with startups and established companies, crafting solutions that elevate user experiences and increase engagement across both digital and physical interfaces.' }
+        { title: '纯真时期', bio: '2013-2024', text: '曾是群聊团宠，用“奥特曼卡片外交”称霸小区，把暑假作业藏冰箱谎称“知识需要冷藏保鲜”，因坚信自己是哈利波特中国分校落榜生而绝食三天。群友至今仍在用他的童年语音包（“姐姐你好像会发光的粽子哦”）当起床闹铃。' },
+        { title: '叛逆时期', bio: '2024-2025', text: '染过七天掉色的荧光紫刘海，在班群发《论食堂鸡腿缩水与资本剥削的关联性》被校长约谈，深夜转发伤痛文学配文“宇宙是块过期蛋糕，而我是上面的霉”。群友为挽救其社交生命值，众筹编写《高中生社会入门手册》，却被他改成《如何科学地让全世界觉得你死了但没完全死》。' },
+        { title: '入定时期', bio: '2025-Now', text: '突然从“45度仰卧起坐”进化为“恒温37度正常生物”，发明“男同学的大腿比女同学更软”理论，在月考作文写《论早自习与蒙德骑士练习的相似性》获零分并登上校园暗网热搜。面对群友咆哮“天天谈恋爱怎么办”，淡定回应：“我给烤冷面摊王叔当互联网军师，三年内上市，饿不死。”群友锐评：“他活成了我老板买不起的样子。”' }
     ],
 
     // 首页用户测评区块
@@ -84,7 +84,7 @@ const CONFIG = {
         '数百位群友共同见证诣宝的成长瞬间！从「24小时发癫」的暖心陪伴，到「轰炸式南梁营业」的欢乐名场面，大家通过每日打卡、表情包互动和深夜畅聊，在欢笑中记录了无数成长里程碑——',
 
     // 用户测评处的跳转按钮
-    PROXIO_TESTIMONIALS_BUTTON_URL: 'https://mvwebfs.tx.kugou.com/202504221939/755223231bad91ad70c4f9c6987ecd7f/v2/660e59b94afdab0c95dc7b7e9ec21a57/KGTX/CLTX002/660e59b94afdab0c95dc7b7e9ec21a57.mp4',
+    PROXIO_TESTIMONIALS_BUTTON_URL: 'https://wapyyk.39.net/hospitals/nanxingbing/c_p1/', // 男科
     PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
 
     // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
@@ -100,47 +100,47 @@ const CONFIG = {
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
+                '诣宝经常抱他裸着的哥们',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
-                'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
-            PROXIO_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
+                '/public/images/gunfu.jpg',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '棍父',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '东京不知名高中生',
+            PROXIO_TESTIMONIALS_ITEM_URL: 'https://zh.moegirl.org.cn/%E7%94%B7%E5%AD%90%E9%AB%98%E4%B8%AD%E7%94%9F%E7%9A%84%E6%97%A5%E5%B8%B8'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
+                '诣宝不是宝贝，是真沙贝',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
-                '/avatar.png',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
-            PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
+                '/public/images/litaton.jpg',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '丽塔欧恩 蛋赞先生',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '超比养老院门口坐着的百岁大爷',
+            PROXIO_TESTIMONIALS_ITEM_URL: 'https://i0.hdslb.com/bfs/archive/d6cf835c590379db77703b13831f9fe9279879fc.jpg'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
+                '他（诣宝）真像个小美女，就是不喜欢刮毛',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
-                'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
-            PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
+                '/public/images/hognxia.jpg',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'ICE',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '下北泽starry吧台の乐队皇帝',
+            PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.bilibili.com/bangumi/play/ss43164'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
-                '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
+                '诣宝他本质是低电的、悲观的、成为自己的英雄。',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
-                '/avatar.png',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
-            PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
+                '/public/images/wuya.jpg',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '群乌鸦-星袅',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '世外神人，答辩大师，在大雷界中享有很高的声望。',
+            PROXIO_TESTIMONIALS_ITEM_URL: 'https://pic.kts.g.mi.com/3dee90f169b3b6de2c855eddd0ea48805170131967573719558.jpg'
         },
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
             PROXIO_TESTIMONIALS_ITEM_AVATAR:
-                '/avatar.png',
-            PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
-            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
-            PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
+                '/Account cancelled.jpg',
+            PROXIO_TESTIMONIALS_ITEM_NICKNAME: '不愿透露姓名的群友',
+            PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '隐私设置，暂不展示',
+            PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.baidu.com/'
         }
     ],
 
@@ -148,12 +148,12 @@ const CONFIG = {
     PROXIO_FAQ_ENABLE: true, // 常见问题模块开关
     PROXIO_FAQ_TITLE: '常见问题解答',
     PROXIO_FAQ_TEXT_1: '有任何问题吗？请看这里',
-    PROXIO_FAQ_TEXT_2: '我们收集了常见的用户疑问',
+    PROXIO_FAQ_TEXT_2: '我们收集了常见的群友疑问',
     PROXIO_FAQS: [
-        { q: 'NotionNext有帮助文档吗？', a: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署' },
-        { q: '部署后要如何编写文章？', a: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>' },
-        { q: '站点部署失败，更新失败？', a: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助' },
-        { q: '文章没有实时同步？', a: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决' },
+        { q: '诣宝几岁了？', a: '诣宝已经3岁零209个月啦，神不知鬼不觉，嗖~！诣宝又升级啦！' },
+        { q: '如何参与记录诣宝的成长？', a: '只需三步，即刻化身“云家长”！1.加入Q群围观每日蹲宝直播 \n2.用截图/记录接龙“投喂”诣宝的成长素材,\n3.参与投票决定下周主题（比如“诣宝的迷惑发言大赏”）\n“你的每条弹幕，都是他的成长弹幕！”' },
+        { q: '诣宝的日记更新频率有多高？', a: '不定时更新记录，但是从现在开始每一天都会有全新的记录与爱同在！' },
+        { q: '素材都是真实的？怀疑有剧本！', a: '本栏目主打一个“人类幼崽不受控”！\n1.证据链：随机抽取群友当“监工”，直击诣宝哭闹现场；2.\n翻车实录：主页专门开设「翻车区」（例：精心策划的大雷被诣宝当成雷坑）\n3.科学认证：特邀幼教专家大冰老师锐评“诣宝迷惑行为”\n“如果这是演的，建议直接保送奥斯卡幼崽组！”' },
     ],
 
     // 关于作者区块
@@ -163,13 +163,13 @@ const CONFIG = {
     PROXIO_ABOUT_TEXT_2:
         '他是群里最受欢迎的“云养崽崽”——诣宝用奶声奶气的语音轰炸、歪歪马马扭扭的游戏录像，和突然蹦出的“人类幼崽迷惑发言”，承包了群友每日的快乐源泉。从第一次摇摇晃晃学会放下，到偷偷把糖果藏在喇叭里，他的成长日记没有滤镜，只有最鲜活的生命力。“看他长大，像养了个电子哈基米，但比猫更费力！”（@每天马枪の群主 含泪认证）',
     PROXIO_ABOUT_PHOTO_URL: '/avatar.png',
-    PROXIO_ABOUT_KEY_1: '经验年限',
-    PROXIO_ABOUT_VAL_1: '10年+',
-    PROXIO_ABOUT_KEY_2: '客户',
+    PROXIO_ABOUT_KEY_1: '成长上限',
+    PROXIO_ABOUT_VAL_1: '未知',
+    PROXIO_ABOUT_KEY_2: '服务过的哥哥姐姐',
     PROXIO_ABOUT_VAL_2: '1000+',
-    PROXIO_ABOUT_KEY_3: '交付项目',
+    PROXIO_ABOUT_KEY_3: '做题总数',
     PROXIO_ABOUT_VAL_3: '5000+',
-    PROXIO_ABOUT_KEY_4: '累积创作时长（小时）',
+    PROXIO_ABOUT_KEY_4: '累积学习时长（小时）',
     PROXIO_ABOUT_VAL_4: '10000+',
 
     PROXIO_ABOUT_BUTTON_URL: 'https://baike.baidu.com/item/%E5%94%90%E7%BA%B3%E5%BE%B7%C2%B7%E7%89%B9%E6%9C%97%E6%99%AE/9916449',
@@ -184,7 +184,7 @@ const CONFIG = {
         'Product Design'
     ],
 
-    PROXIO_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+    PROXIO_FOOTER_SLOGAN: '陪诣宝长大，让时光有温度。',
 
     // 页脚三列菜单组
     // 页脚菜单
@@ -193,34 +193,34 @@ const CONFIG = {
             name: '友情链接',
             menus: [
                 {
-                    title: 'Tangly的学习笔记',
-                    href: 'https://blog.tangly1024.com'
+                    title: 'ZCHBImpulseX',
+                    href: 'https://space.bilibili.com/123704388'
                 },
                 {
-                    title: 'NotionNext',
-                    href: 'https://www.tangly1024.com'
+                    title: '奶龙',
+                    href: 'https://www.bilibili.com/bangumi/play/ss40551'
                 }
             ]
         },
         {
             name: '开发者',
             menus: [
-                { title: 'Github', href: 'https://github.com/tangly1024/NotionNext' },
+                { title: 'Github', href: 'https://github.com' },
                 {
                     title: '开发帮助',
-                    href: 'https://docs.tangly1024.com/article/how-to-develop-with-notion-next'
+                    href: 'https://baijiahao.baidu.com/s?id=1812029965541106189'
                 },
                 {
                     title: '功能反馈',
-                    href: 'https://github.com/tangly1024/NotionNext/issues/new/choose'
+                    href: 'https://tieba.baidu.com/f?kw=%E5%9C%A3%E6%9D%AF%E6%88%98%E4%BA%89&ie=utf-8'
                 },
                 {
                     title: '技术讨论',
-                    href: 'https://github.com/tangly1024/NotionNext/discussions'
+                    href: 'https://tieba.baidu.com/f?kw=apex'
                 },
                 {
                     title: '关于作者',
-                    href: 'https://blog.tangly1024.com/about'
+                    href: 'https://baike.baidu.com/item/%E7%9A%AE%E7%89%B9%C2%B7%E6%A0%BC%E9%87%8C%E8%8A%AC/64564737'
                 }
             ]
         }],
@@ -243,17 +243,17 @@ const CONFIG = {
 
     // 页面底部的行动呼吁模块
     PROXIO_CTA_ENABLE: true,
-    PROXIO_CTA_TITLE: '与我建立联系',
-    PROXIO_CTA_TITLE_2: '让我们立刻启动您的项目',
+    PROXIO_CTA_TITLE: '与诣宝建立联系',
+    PROXIO_CTA_TITLE_2: '让我们离诣宝更进一步',
     PROXIO_CTA_DESCRIPTION:
-        '访问NotionNext的操作文档，我们提供了详细的教程，帮助你即刻搭建站点',
+        '我们提供了详细的记录，帮助你即刻闯入诣宝生活',
     PROXIO_CTA_BUTTON: true, // 是否显示按钮
     PROXIO_CTA_BUTTON_URL:
-        '/about',
+        'https://wapyyk.39.net/hospitals/nanxingbing/c_p1/',
     PROXIO_CTA_BUTTON_TEXT: '联系我',
 
     PROXIO_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
-    PROXIO_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
+    PROXIO_POST_REDIRECT_URL: 'https://wapyyk.39.net/hospitals/nanxingbing/c_p1/', // 重定向域名
     PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
 }
 export default CONFIG
