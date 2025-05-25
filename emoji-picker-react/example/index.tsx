542ac02694c1c89abeb9f75c3cf2bafdf6ce2c01
LeeHeron <foinyong@gmail.com>
2025-05-26 00:03:13 +0800

    修复3

--- emoji-picker-react/example/index.tsx
+++ emoji-picker-react/example/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { EmojiPicker } from '../.';
+import { EmojiPicker } from 'emoji-picker-react';
 
 const App = () => {
   return (
