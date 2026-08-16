f5e863fe53080e120b2a5f8ff4550709efc3f800
LeeHeron <foinyong@gmail.com>
2026-08-16 13:56:32 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,7 +5,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/govmoe/XuHome-Theme'
+        href='https://www.bilibili.com/video/BV1RuMC6eE8s/?share_source=copy_web&vd_source=bc152f3020fcf928d0cce820575a8692'
         className='underline justify-start'>
         大雷科技
       </a>
