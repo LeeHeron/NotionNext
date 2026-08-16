24a536e00ce3f66caecd9ed6b7a408d28cb23847
LeeHeron <foinyong@gmail.com>
2026-08-16 13:48:16 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,17 +1,13 @@
 import { siteConfig } from '@/lib/config'
 
-/**
- * 驱动版权
- * @returns
- */
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://github.com/govmoe/XuHome-Theme'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        大雷科技
       </a>
       .
     </div>
